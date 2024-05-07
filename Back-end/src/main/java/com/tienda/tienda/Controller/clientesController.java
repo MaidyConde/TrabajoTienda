26a1cd26573b58d1d322eef1cd8c0dc644c3bef3
Maidy Conde <maidyladino04@gmail.com>
2024-05-07 13:24:33 -0500

    dhfgyegf

--- Back-end/src/main/java/com/tienda/tienda/Controller/clientesController.java
+++ Back-end/src/main/java/com/tienda/tienda/Controller/clientesController.java
@@ -1,5 +1,12 @@
 package com.tienda.tienda.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/Clientes/")
+@RestController
 public class clientesController {
+
+
 
 }
